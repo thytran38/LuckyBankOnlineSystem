e253cebc131dde9689944be9d3397684e58105dd
tri-nhan <nhan0385790927@gmail.com>
2020-11-24 09:18:35 +0700

    added WaitingDialog

--- app/src/main/java/com/example/myfirstapp/luckybankonlinesystem/Fragment/WaitingDialog.java
+++ app/src/main/java/com/example/myfirstapp/luckybankonlinesystem/Fragment/WaitingDialog.java
@@ -13,15 +13,32 @@ import androidx.fragment.app.DialogFragment;
 import com.airbnb.lottie.LottieAnimationView;
 import com.airbnb.lottie.LottieDrawable;
 import com.example.myfirstapp.luckybankonlinesystem.R;
+import com.google.android.gms.tasks.Task;
 
 import java.util.Objects;
 
 public class WaitingDialog extends DialogFragment {
+
     private final int waitingAnimationId;
+    private int waitingTime;
+    private Task waitingTask;
+    private LottieAnimationView animationView;
+
 
     public WaitingDialog(int waitingAnimationId) {
-        this.waitingAnimationId = waitingAnimationId;
         this.setCancelable(false);
+        this.waitingAnimationId = waitingAnimationId;
+        this.waitingTime = 5;
+    }
+
+    public WaitingDialog(int waitingAnimationId, int waitingTime) {
+        this(waitingAnimationId);
+        this.waitingTime = waitingTime;
+    }
+
+    public WaitingDialog(int waitingAnimationId, Task waitingTask) {
+        this(waitingAnimationId);
+        this.waitingTask = waitingTask;
     }
 
     @NonNull
@@ -29,13 +46,22 @@ public class WaitingDialog extends DialogFragment {
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         LayoutInflater inflater = Objects.requireNonNull(getActivity()).getLayoutInflater();
         final View view = inflater.inflate(R.layout.loading_layout, null);
-        LottieAnimationView animationView = view.findViewById(R.id.animation_container);
+        animationView = view.findViewById(R.id.animation_container);
         animationView.setAnimation(waitingAnimationId);
         animationView.setRepeatMode(LottieDrawable.REVERSE);
+        if (waitingTask != null) {
+            waitingTask.addOnCompleteListener(task -> cancel());
+        } else {
+            animationView.postDelayed(this::cancel, waitingTime * 1000);
+        }
         return new AlertDialog.Builder(Objects.requireNonNull(getContext()))
                 .setView(view)
                 .setCancelable(false)
                 .create();
     }
-}
 
+    private void cancel() {
+        animationView.cancelAnimation();
+        this.dismiss();
+    }
+}
