6f64632dfbe5234ae947206a58cd4f93a47118f5
thytran38 <ax4409h@gmail.com>
2020-11-27 16:30:30 +0700

    add some lines of codes

--- app/src/main/java/com/example/myfirstapp/luckybankonlinesystem/LoginActivity.java
+++ app/src/main/java/com/example/myfirstapp/luckybankonlinesystem/LoginActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +13,7 @@ public class LoginActivity extends AppCompatActivity {
 
     EditText userName, password;
     Button login, register_login;
+    TextView register, forgetPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,15 +23,35 @@ public class LoginActivity extends AppCompatActivity {
         userName = (EditText)findViewById(R.id.txtUserName);
         password = (EditText)findViewById(R.id.txtPassword);
         login = (Button)findViewById(R.id.btnLogin);
-        register_login = (Button)findViewById(R.id.btnRegister_Login);
+        register = (TextView)findViewById(R.id.tvRegister);
+        forgetPass = (TextView)findViewById(R.id.tvForgotPassword);
 
-        register_login.setOnClickListener(new View.OnClickListener() {
+        register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
                 startActivity(intent);
+
+
             }
         });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        forgetPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent forgetPassIntent = new Intent(this,ForgetPassActivity.class);
+
+
+            }
+        });
+
 
 
     }
