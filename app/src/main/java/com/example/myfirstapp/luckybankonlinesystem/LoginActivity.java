e80b0a29d3a191ba56f313d7c3f0084b114661bd
thytran38 <ax4409h@gmail.com>
2020-11-30 12:18:28 +0700

    add SplashScreen

--- app/src/main/java/com/example/myfirstapp/luckybankonlinesystem/LoginActivity.java
+++ app/src/main/java/com/example/myfirstapp/luckybankonlinesystem/LoginActivity.java
@@ -18,7 +18,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.home);
 
         userName = (EditText)findViewById(R.id.txtUserName);
         password = (EditText)findViewById(R.id.txtPassword);
