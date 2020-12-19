57d4b2c71e059bc44b71f6589f40b0377bf41824
thytran38 <ax4409h@gmail.com>
2020-12-19 11:22:11 +0700

    re-add firestore

--- app/src/main/java/com/example/myfirstapp/luckybankonlinesystem/Model/Accounts.java
+++ app/src/main/java/com/example/myfirstapp/luckybankonlinesystem/Model/Accounts.java
@@ -1,46 +1,92 @@
-package com.example.myfirstapp.luckybankonlinesystem.Model;
-//import com.example.myfirstapp.luckybankonlinesystem.Class.DateTime;
-
-import com.google.type.DateTime;
-
-import java.util.HashSet;
-
-public class Accounts {
-    private String AccountNumber;
-    private DateTime DateCreated;
-    HashSet<Accounts> accountList;
-    private int Account_Type;
-    private String Account_Owner;
-
-    public Accounts()
-    {
-
-    }
-
-//    public HashSet<Accounts> getAccountList(){
+//package com.example.myfirstapp.luckybankonlinesystem.Model;
+////import com.example.myfirstapp.luckybankonlinesystem.Class.DateTime;
 //
-//        HashSet<Accounts> thisList ;
-//        return thisList;
+//import com.google.type.DateTime;
+//
+//import java.util.HashSet;
+//
+//public class Accounts {
+//    private String AccountNumber;
+//    private DateTime DateCreated;
+//    HashSet<Accounts> accountList;
+//    private int Account_Type;
+//    private String Account_Owner;
+//
+//    public Accounts()
+//    {
+//
 //    }
-
-    public String getAccountNumber(){
-
-        return this.AccountNumber;
-    }
-
-    public DateTime getDateCreated()
-    {
-        return this.DateCreated;
-    }
-
-    public int getAccountType()
-    {
-        return this.Account_Type;
-    }
-
-    public String getAccountOwner()
-    {
-        return this.Account_Owner;
-    }
-
-}
+//
+////    public HashSet<Accounts> getAccountList(){
+////
+////        HashSet<Accounts> thisList ;
+////        return thisList;
+////    }
+//
+//    public String getAccountNumber(){
+//
+//        return this.AccountNumber;
+//    }
+//
+//    public DateTime getDateCreated()
+//    {
+//        return this.DateCreated;
+//    }
+//
+//    public int getAccountType()
+//    {
+//        return this.Account_Type;
+//    }
+//
+//    public String getAccountOwner()
+//    {
+//        return this.Account_Owner;
+//    }
+//
+//}
+//package com.example.myfirstapp.luckybankonlinesystem.Model;
+////import com.example.myfirstapp.luckybankonlinesystem.Class.DateTime;
+//
+//import com.google.type.DateTime;
+//
+//import java.util.HashSet;
+//
+//public class Accounts {
+//    private String AccountNumber;
+//    private DateTime DateCreated;
+//    HashSet<Accounts> accountList;
+//    private int Account_Type;
+//    private String Account_Owner;
+//
+//    public Accounts()
+//    {
+//
+//    }
+//
+////    public HashSet<Accounts> getAccountList(){
+////
+////        HashSet<Accounts> thisList ;
+////        return thisList;
+////    }
+//
+//    public String getAccountNumber(){
+//
+//        return this.AccountNumber;
+//    }
+//
+//    public DateTime getDateCreated()
+//    {
+//        return this.DateCreated;
+//    }
+//
+//    public int getAccountType()
+//    {
+//        return this.Account_Type;
+//    }
+//
+//    public String getAccountOwner()
+//    {
+//        return this.Account_Owner;
+//    }
+//
+//}
